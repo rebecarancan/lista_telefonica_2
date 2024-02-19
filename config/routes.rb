0df4d37dfe7881aef81c0b4632f12e5d6d6cafce
Rebeca Rancan <rebecarancanrr@gmail.com>
2024-02-19 09:28:07 -0300

    add contacts controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "contacts#index"
+
+  get "/contacts", to: "contacts#index"
+  post "/contacts", to: "contacts#create"
+  get "/contacts/new", to: "contacts#new", as: "new_contact"
+  get "/contacts/:id", to: "contacts#show", as: "contact"
+  get "contacts/:id/edit", to: "contacts#edit", as: "edit_contact"
+  patch "contacts/:id", to: "contacts#update"
+  delete "contacts/:id", to: "contacts#destroy"
 end
